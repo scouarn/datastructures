5cd3afb649726b5285411a3cb7ed7d4b5a258f42
scouarn <scouarn.contact@gmail.com>
2021-11-09 22:17:30 +0100

    matix

--- include/matrix.h
+++ include/matrix.h
@@ -1,50 +1,27 @@
 #ifndef _MATRIX_H_
 #define _MATRIX_H_
 
-typedef struct __M_mat_t__ M_mat_t;
-typedef struct __M_vec_t__ M_vec_t;
+typedef double* M_mat_t;
 
-M_vec_t* M_vec_make(int dim);
 M_mat_t* M_mat_make(int rows, int cols);
-
-void M_vec_free(M_vec_t* vec);
 void M_mat_free(M_mat_t* mat);
 
-void M_vec_print(M_vec_t* vec);
-void M_mat_print(M_mat_t* mat);
+/* if M_mat_free used on a matrix made with this function,
+ * data will also be freeed. To avoid that just use free(mat). */
+M_mat_t* M_mat_fromArray(double* data, int rows, int cols);
 
+void M_mat_size(M_mat_t* mat, int* rows, int* cols);
+void M_mat_print(const char* format, M_mat_t* mat);
 
-double M_vec_dot   (M_vec_t* u, M_vec_t* v);
-double M_vec_normsq(M_vec_t* vec);
-double M_vec_norm  (M_vec_t* vec);
-
-double M_mat_trace (M_mat_t* mat);
-double M_mat_det   (M_mat_t* mat);
-
-
-void M_vec_copy (M_vec_t* dst, M_vec_t* src);
-void M_vec_add  (M_vec_t* dst, M_vec_t* u, M_vec_t* v);
-void M_vec_scale(M_vec_t* dst, M_vec_t* vec, double a);
-
-void M_extract_row(M_vec_t* dst, M_mat_t* mat);
-void M_extract_col(M_vec_t* dst, M_mat_t* mat);
-
-
+void M_mat_zero(M_mat_t* dst);
+void M_mat_unit(M_mat_t* dst);
 void M_mat_copy (M_mat_t* dst, M_mat_t* src);
 void M_mat_add  (M_mat_t* dst, M_mat_t* a, M_mat_t* b);
 void M_mat_scale(M_mat_t* dst, M_mat_t* mat, double a);
+void M_mat_trans(M_mat_t* dst, M_mat_t* mat);
+void M_mat_mul(M_mat_t* dst, M_mat_t* a, M_mat_t* b);
 
-
-void M_mat_transpose(M_mat_t* dst, M_mat_t* mat);
-void M_mat_invert   (M_mat_t* dst, M_mat_t* mat);
-void M_linsolve	    (M_mat_t* dst, M_mat_t* mat, M_mat_t* vec);
-
-
-void M_mat_ab  (M_mat_t* dst, M_mat_t* a, M_mat_t* b);
-void M_mat_aTb (M_mat_t* dst, M_mat_t* a, M_mat_t* b);
-void M_mat_abT (M_mat_t* dst, M_mat_t* a, M_mat_t* b);
-void M_mat_aTbT(M_mat_t* dst, M_mat_t* a, M_mat_t* b);
-#define M_mat_mul M_mat_ab
 
 
 #endif /* matrix_h */
+
---
+++ source/matrix.c
@@ -0,0 +1,224 @@
+#include "matrix.h"
+#include "errors.h"
+#include <stdlib.h>
+
+
+
+
+M_mat_t* M_mat_make(int rows, int cols) {
+
+	int i;
+	M_mat_t* mat = malloc( rows * sizeof(double*) + 2*sizeof(int) );
+	
+	/* hide size in the first two cells of array */
+	mat += 2;
+	mat[-1] = (M_mat_t)rows;
+	mat[-2] = (M_mat_t)cols;
+
+	mat[0] = malloc( rows * cols * sizeof(double) );
+
+	for (i = 1; i < rows; i++)
+		mat[i] = mat[0] + i*cols;
+
+	return (M_mat_t*)mat;
+}
+
+M_mat_t* M_mat_fromArray(double* data, int rows, int cols) {
+
+	int i;
+	M_mat_t* mat = malloc( rows * sizeof(double*) + 2*sizeof(int) );
+	
+	/* hide size in the first two cells of array */
+	mat += 2;
+	mat[-1] = (M_mat_t)rows;
+	mat[-2] = (M_mat_t)cols;
+
+	mat[0] = data;
+
+	for (i = 1; i < rows; i++)
+		mat[i] = data + i*cols;
+
+	return (M_mat_t*)mat;
+}
+
+
+void M_mat_free(M_mat_t* mat) {
+	free(mat[0]);
+	free(mat-2);
+}
+
+void M_mat_size(M_mat_t* mat, int* rows, int* cols) {
+	*rows = (int)mat[-1];
+	*cols = (int)mat[-2];
+}
+
+
+void M_mat_print(const char* format, M_mat_t* mat) {
+	int i, j, rows, cols;
+	M_mat_size(mat, &rows, &cols);
+
+	for (i = 0; i < rows; i++) {
+
+		printf("[ ");
+
+		for (j = 0; j < cols; j++)
+			printf(format, mat[i][j]);
+
+		printf(" ]\n");
+	}
+
+}
+
+void M_mat_zero(M_mat_t* mat) {
+	int i, j, rows, cols;
+	M_mat_size(mat, &rows, &cols);
+
+	for (i = 0; i < rows; i++)
+	for (j = 0; j < cols; j++)
+		mat[i][j] = 0.0;
+}
+
+
+void M_mat_unit(M_mat_t* mat) {
+	int i, rows, cols;
+	M_mat_size(mat, &rows, &cols);
+
+	M_assert(rows == cols, "Identity matrix must be square.");
+
+	for (i = 0; i < rows; i++)
+		mat[i][i] = 1.0;
+}
+
+
+void M_mat_copy(M_mat_t* dst, M_mat_t* src) {
+	int i, j, src_r, src_c, dst_r, dst_c;
+	M_mat_size(src, &src_r, &src_c);
+	M_mat_size(dst, &dst_r, &dst_c);
+
+	M_assert(src_r == dst_r && src_c == dst_c, 
+		"dst and src must be same size.");
+
+	for (i = 0; i < dst_r; i++)
+	for (j = 0; j < dst_c; j++)
+		dst[i][j] = src[i][j];
+}
+
+
+
+void M_mat_add(M_mat_t* dst, M_mat_t* a, M_mat_t* b) {
+	int i, j, a_r, a_c, b_r, b_c, dst_r, dst_c;
+	M_mat_t* tmp;
+
+	M_mat_size(a, &a_r, &a_c);
+	M_mat_size(b, &b_r, &b_c);
+	M_mat_size(dst, &dst_r, &dst_c);
+
+	M_assert(a_r == dst_r && a_c == dst_c, 
+		"dst and a must be same size.");
+
+	M_assert(b_r == dst_r && b_c == dst_c, 
+		"dst and b must be same size.");
+
+
+	if (dst == a || dst == b)
+		tmp = M_mat_make(dst_r, dst_c);
+	else
+		tmp = dst;
+
+
+	for (i = 0; i < dst_r; i++)
+	for (j = 0; j < dst_c; j++)
+		tmp[i][j] = a[i][j] + b[i][j];
+
+
+	if (dst == a || dst == b) {
+		M_mat_copy(dst, tmp);
+		M_mat_free(tmp);
+	}
+
+}
+
+
+void M_mat_scale(M_mat_t* dst, M_mat_t* src, double a) {
+	int i, j, src_r, src_c, dst_r, dst_c;
+	M_mat_size(src, &src_r, &src_c);
+	M_mat_size(dst, &dst_r, &dst_c);
+
+	M_assert(src_r == dst_r && src_c == dst_c, 
+		"dst and src must be same size.");
+
+	for (i = 0; i < dst_r; i++)
+	for (j = 0; j < dst_c; j++)
+		dst[i][j] = a * src[i][j];
+}
+
+
+void M_mat_trans(M_mat_t* dst, M_mat_t* src) {
+	int i, j, src_r, src_c, dst_r, dst_c;
+	M_mat_t* tmp;
+
+	M_mat_size(src, &src_r, &src_c);
+	M_mat_size(dst, &dst_r, &dst_c);
+
+	M_assert(src_r == dst_c && src_c == dst_r, 
+		"Invalid dimensions for transposition.");
+
+
+	if (dst == src)
+		tmp = M_mat_make(dst_r, dst_c);
+	else
+		tmp = dst;
+
+
+	for (i = 0; i < dst_r; i++)
+	for (j = 0; j < dst_c; j++)
+		tmp[i][j] = src[j][i];
+
+
+	if (dst == src) {
+		M_mat_copy(dst, tmp);
+		M_mat_free(tmp);
+	}
+
+}
+
+
+void M_mat_mul(M_mat_t* dst, M_mat_t* a, M_mat_t* b) {
+	int i, j, k, a_r, a_c, b_r, b_c, dst_r, dst_c;
+	M_mat_t* tmp;
+
+	M_mat_size(a, &a_r, &a_c);
+	M_mat_size(b, &b_r, &b_c);
+	M_mat_size(dst, &dst_r, &dst_c);
+
+	M_assert(a_c == b_r, 
+		"a must have as much cols as b has rows.");
+
+	M_assert(dst_r == a_r, 
+		"dst must have as much rows as a.");
+
+	M_assert(dst_c == b_c, 
+		"dst must have as much cols as b.");
+
+
+	if (dst == a || dst == b)
+		tmp = M_mat_make(dst_r, dst_c);
+	else
+		tmp = dst;
+
+
+	for (i = 0; i < dst_r; i++)
+	for (j = 0; j < dst_c; j++) {
+
+		tmp[i][j] = 0;
+		for (k = 0; k < a_c; k++)
+			tmp[i][j] += a[i][k] * b[k][j];
+	}
+
+
+	if (dst == a || dst == b) {
+		M_mat_copy(dst, tmp);
+		M_mat_free(tmp);
+	}
+
+}
